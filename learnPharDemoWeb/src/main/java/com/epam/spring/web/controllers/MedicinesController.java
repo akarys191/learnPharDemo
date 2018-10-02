6d392cb37e521da787aef1cbbd2d6c88b840ce1b
akarys191 <akarys191@gmail.com>
2018-10-02 19:53:07 +0600

    refactoring to multiple maven projects

--- learnPharDemoWeb/src/main/java/com/epam/spring/web/controllers/MedicinesController.java
+++ learnPharDemoWeb/src/main/java/com/epam/spring/web/controllers/MedicinesController.java
@@ -1,14 +1,23 @@
 package com.epam.spring.web.controllers;
 
+import com.epam.spring.demo.services.MedicineService;
+import com.epam.spring.demo.services.SupplierService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MedicinesController {
+    private final MedicineService medicineService;
+
+    public MedicinesController(MedicineService medicineService) {
+        this.medicineService = medicineService;
+    }
 
     @RequestMapping({"/medicines/"})
-    public String listSuppliers(){
-       // MedicineServiceMap medicineServiceMap = new MedicineServiceMap();
+    public String listSuppliers(Model model){
+        model.addAttribute("medicines", medicineService.findAll() );
+
         return "medicines/medicines";
     }
 }
--- learnPharDemoWeb/src/main/java/com/epam/spring/web/controllers/SuppliersController.java
+++ learnPharDemoWeb/src/main/java/com/epam/spring/web/controllers/SuppliersController.java
@@ -1,14 +1,21 @@
 package com.epam.spring.web.controllers;
 
+import com.epam.spring.demo.services.SupplierService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SuppliersController {
 
+    private final SupplierService supplierService;
+
+    public SuppliersController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
     @RequestMapping({"suppliers/"})
-    public String listSuppliers(){
-       // MedicineServiceMap medicineServiceMap = new MedicineServiceMap();
+    public String listSuppliers(Model model){
+        model.addAttribute("suppliers", supplierService.findAll() );
         return "suppliers/suppliers";
     }
 }
