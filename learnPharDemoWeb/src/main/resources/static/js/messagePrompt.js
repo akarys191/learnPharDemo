fd95f182914ed6fdb97b9770869cc6e3e0881441
akarys191 <akarys191@gmail.com>
2019-08-11 19:26:45 +0600

    fixed fontawesome css & js issues

--- learnPharDemoWeb/src/main/resources/static/js/messagePrompt.js
+++ learnPharDemoWeb/src/main/resources/static/js/messagePrompt.js
@@ -2,7 +2,7 @@ function messagePrompt(msg){
       $( function() {
           bootbox.alert({
              message: msg,
-              size: 'small',
+             /* size: 'small',*/
               locale:'ru'
            }).css({'font-weight' : 'bold',  'color': '#F00', 'font-size': '22px', 'font-weight' : 'bold'} );;
       });
