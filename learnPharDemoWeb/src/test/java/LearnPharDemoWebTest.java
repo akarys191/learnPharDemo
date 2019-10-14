6acc7dd6cb207f7a86e7cd669234fe6f1f1894a0
akarys191 <akarys191@gmail.com>
2019-10-14 20:46:34 +0600

    Issue 8-10 Added some tests

--- learnPharDemoWeb/src/test/java/LearnPharDemoWebTest.java
+++ learnPharDemoWeb/src/test/java/LearnPharDemoWebTest.java
@@ -1,46 +1,54 @@
 /*
-import com.pharm.demo.web.config.WebConfig;
-import org.junit.Before;
+import com.pharm.demo.LearnPharDemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@RunWith(SpringRunner.class)
+*/
+/*@RunWith(SpringRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes= WebConfig.class)
+@AutoConfigureCache*//*
+
+ */
+/*@SpringBootTest(classes = WebConfig.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(secure = false)
+@ContextConfiguration(classes = WebConfig.class)*//*
+
+ */
+/*@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = { IndexMvcController.class })
+@ContextConfiguration(classes = WebConfig.class)
+@AutoConfigureCache*//*
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LearnPharDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class LearnPharDemoWebTest {
 
 
-     @Autowired
+    @Autowired
     private WebApplicationContext wac;
+
+    @Autowired
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() {
-        DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
-        this.mockMvc = builder.build();
-    }
-
     @Test
-    public void testHomePage() throws Exception {
+    public void testLoginPage() throws Exception {
         System.out.println("testing.................");
-        this.mockMvc.perform(get("/homepage"))
-                .andExpect(view().name("home"));
+        Object result = mockMvc.perform(get("/index"));
+        this.mockMvc.perform(get("/index"))
+                .andExpect(content().string(containsString("index")));
     }
-
-
-
-
 
 }
 
