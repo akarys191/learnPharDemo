23961f377ad64bec15f01d338ceb8b2ad181a7ea
akarys191 <akarys191@gmail.com>
2019-10-14 23:19:27 +0600

    issue-8-8 Test

--- learnPharDemoWeb/src/main/java/com/pharm/demo/web/controllers/mvc/IndexMvcController.java
+++ learnPharDemoWeb/src/main/java/com/pharm/demo/web/controllers/mvc/IndexMvcController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("/")
 public class IndexMvcController {
 
-    @RequestMapping({"","/","index","index.html"})
+    @GetMapping({"", "/", "/index", "index", "index.html"})
     public String index(){
         return "index";
     }
