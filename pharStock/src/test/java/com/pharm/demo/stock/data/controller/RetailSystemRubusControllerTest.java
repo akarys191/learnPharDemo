789000f19fc46ff4dd5289edc72784307cf873ba
Tokmurzin <mukagali.2002.05@gmail.com>
2021-11-27 17:19:52 +0600

    Rest Controller test, fixed

--- pharStock/src/test/java/com/pharm/demo/stock/data/controller/RetailSystemRubusControllerTest.java
+++ pharStock/src/test/java/com/pharm/demo/stock/data/controller/RetailSystemRubusControllerTest.java
@@ -1,47 +1,38 @@
 package com.pharm.demo.stock.data.controller;
 
 import com.pharm.demo.stock.data.model.RetailSystemStock;
-import com.pharm.demo.stock.data.repository.RetailSystemStockRepository;
 import com.pharm.demo.stock.data.service.RetailSystemStockFillingService;
 import com.pharm.demo.stock.data.service.RetailSystemStockService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.util.List;
+import java.math.BigInteger;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = RetailSystemRubusController.class)
 public class RetailSystemRubusControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-
-    @InjectMocks
-    private RetailSystemStockService retailSystemRubusService;
-
     @MockBean
-    private List<RetailSystemStock> list;
+    private RetailSystemStockFillingService retailSystemStockFillingService;
+    @MockBean
+    private RetailSystemStockService retailSystemStockService;
 
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void testFindAll() throws Exception {
@@ -49,12 +40,24 @@ public class RetailSystemRubusControllerTest {
     }
 
     @Test
-    public void testFindById() {
+    public void testFindById() throws Exception {
+        BigInteger id = BigInteger.valueOf(123);
+
+        RetailSystemStock retailSystemStock = new RetailSystemStock();
+        retailSystemStock.set_id(id);
+        retailSystemStock.setBarCode("123456789");
+
+        when(retailSystemStockService.findById(id)).thenReturn(retailSystemStock);
+        mvc.perform(get("/{id}", id)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._id", is(123)))
+                .andExpect(jsonPath("$.barCode", is("123456789")));
     }
 
     @Test
-    public void testUploadFile() {
-
+    public void testUploadFile() throws Exception {
+        mvc.perform(post("/upload")).andDo(print())
+                .andExpect(status().isOk());
     }
 }
 
